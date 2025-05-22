d03e972a85ed992f86b78e7740ced8d2baf783de
Checky23 <aurita.vero2005@gmail.com>
2025-05-22 13:46:24 -0600

    Agrege las librerias de ConstrainLayaout, la de NavHost para navegacion de pantallas, y parte de las librerias de firebase (no lo envio completamento todo de firebase)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,6 +43,11 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.9.0"
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-beta01")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
