868011c6871877d9da5c5bf4638a7398cff91f0c
Checky23 <aurita.vero2005@gmail.com>
2025-05-22 13:31:46 -0600

    Elimine las funciones por defecto del main y cree nuevas funciones de preview y main

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -11,37 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.login.ui.theme.LoginTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LoginTheme {
-        Greeting("Android")
-    }
+fun PreviewMainScreen() {
+
 }
